f5691a2b7c262494d5eab5c4637c1f3ce39cf0a1
lipeng <469059809@qq.com>
2025-04-22 22:15:29 +0800

    pyqt

--- python_test/pyqt5视频学习/03-QObject定时器.py
+++ python_test/pyqt5视频学习/03-QObject定时器.py
@@ -1,10 +1,7 @@
 import sys
-
-from PyQt5.QtCore import QObject, Qt
+from PyQt5.QtCore import QObject
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel
 
-txt= 10
-vua=0
 
 class My_object(QObject):
     def timerEvent(self, evt):
@@ -28,6 +25,26 @@ class My_Qlabel(QLabel):
             self.killTimer(self.timer_id)
     def set_va(self,va):
         self.setText(va)
+
+class My_Qlabel2(QLabel):
+    def __init__(self,*args,**kwargs):
+        super().__init__(*args,**kwargs)
+        self.setText("10")
+        self.move(100,100)
+        self.setStyleSheet("color: red")
+    def set_my_timer(self,s):
+        self.timer_id = self.startTimer(s)
+    def timerEvent(self,*args,**kwargs):
+        print("xxx")
+        value=int(self.text())
+        value-=1
+        self.setText(str(value))
+
+        if value ==0:
+            print("停止le")
+            self.killTimer(self.timer_id)
+    def set_va(self,va):
+        self.setText(va)
 #1.创建一个应用程序对象
 app=QApplication(sys.argv)
 
@@ -35,17 +52,16 @@ app=QApplication(sys.argv)
 #2.1创建控件
 window=QWidget()
 
-
 #2.1设置控件
 window.setWindowTitle("定时器的使用")
 window.resize(500,500)
 
 label= My_Qlabel(window)
-label.set_va("99")
-label.set_my_timer(5000)
+label.set_va("10")
+label.set_my_timer(1000)
 
-label2= My_Qlabel(window)
-label2.set_va("99")
+label2= My_Qlabel2(window)
+label2.set_va("10")
 label2.set_my_timer(1000)
 label2.move(20,20)
 
