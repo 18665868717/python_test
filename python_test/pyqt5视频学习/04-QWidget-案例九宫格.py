0ea263e9314089e80af73a18b6d35e46d75288fd
lipeng <469059809@qq.com>
2025-04-23 22:09:14 +0800

    pyqt

---
+++ python_test/pyqt5视频学习/04-QWidget-案例九宫格.py
@@ -0,0 +1,32 @@
+import sys
+
+from PyQt5.QtWidgets import QWidget, QApplication
+
+app =QApplication(sys.argv)
+
+window=QWidget()
+window.show()
+window.resize(500,500)
+window.move(300,300)
+
+
+#总控件的个数
+widget_count= 20
+#一行有多少控件
+column_count = 3
+#计算一个控件的宽度
+widget_width = int(window.width() / column_count)
+#总共有多少行 （编号 // 一行多少列 + 1）
+row_count=(widget_count - 1) // column_count + 1
+widget_height = int(window.height() / row_count)
+
+for i in range(0,widget_count):
+    w=QWidget(window)
+    w.resize(widget_width , widget_height)
+    widget_x = i % column_count * widget_width
+    widget_y = i // column_count * widget_height
+    w.move(widget_x,widget_y)
+    w.setStyleSheet("background-color: red;border: 1px solid yellow;")
+    w.show()
+
+sys.exit(app.exec())
--- python_test/pyqt5视频学习/04-QWidget.py
+++ python_test/pyqt5视频学习/04-QWidget.py
@@ -8,18 +8,33 @@ app=QApplication(sys.argv)
 #2.1创建控件
 window=QWidget()
 
-
+"""
+move(x,y)
+resize(width,height)
+setGeometry(x,y,width,height)
+adjustSize()  根据自适应大小
+setFixedSize(x，y) 设置固定大小
+"""
 #2.1设置控件
-window.setWindowTitle("")
-window.resize(500,500)
+window.move(200,100) #移动桌面左上100，100
+# window.resize(500,500)
+window.setFixedSize(500,500)  #固定大小，不能放大和缩小
+# window.setGeometry(0,0,200,200)#用户区域，不包含标题栏
 
-print(QWidget.__bases__)
-print(QWidget.mro())
 
-red=QWidget(window)
-red.resize(100,100)
-red.setStyleSheet("background-color: red;")
-red.move(300,0)
+label = QLabel(window)
+label.setText("文本")
+label.move(100,100)
+label.setStyleSheet("background-color: cyan;")
+def changecao():
+    new_content=label.text()+ "文本"
+    label.setText(new_content)
+    label.resize(label.width()+100,label.height())
+
+but = QPushButton(window)
+but.setText("anniu")
+but.move(100,300)
+but.clicked.connect(changecao)
 
 #2.3展示控件
 window.show()
