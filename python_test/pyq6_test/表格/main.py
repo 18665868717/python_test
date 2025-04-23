eeaee5ed07a558a6c9176dfb85f9307f49ac0e93
lizepeng <469059809@qq.com>
2025-04-23 17:48:53 +0800

    pyqt

--- python_test/pyq6_test/表格/main.py
+++ python_test/pyq6_test/表格/main.py
@@ -2,8 +2,6 @@ import sys
 import PyQt5.QtWidgets as qw
 import PyQt5.QtCore as qc
 
-from PyQt6.QtCore import Qt
-
 import tabel_ui
 
 class My_Books(qw.QMainWindow,tabel_ui.Ui_Form):
@@ -15,10 +13,22 @@ class My_Books(qw.QMainWindow,tabel_ui.Ui_Form):
     def add_data(self):
         book_list=[["1","辰东","完美世界","2","经典"],["12","辰东","遮天","1","经典"]]
         self.tableWidget.setRowCount(len(book_list))  # 设置行数
+        self.tableWidget.verticalHeader().setVisible(False)  # 隐藏行头
+        self.tableWidget.setAlternatingRowColors(True) #间隔一行换个颜色
+
+        # self.tableWidget.resizeColumnsToContents #自适应宽度
+        self.tableWidget.setColumnWidth(0, 50)  # ID
+        self.tableWidget.setColumnWidth(1, 120)  # 作者
+        self.tableWidget.setColumnWidth(2, 200)  # 书名
+        self.tableWidget.setColumnWidth(3, 80)  # 剩余数
+        self.tableWidget.setColumnWidth(4, 200)  # 评论
 
         for row, book in enumerate(book_list):
+            print("第一次遍历",row,book)
             for col, data in enumerate(book):
+                print("第二次遍历",col,data)
                 item = qw.QTableWidgetItem(data)
+                item.setTextAlignment(qc.Qt.AlignCenter)
                 self.tableWidget.setItem(row, col, item)
 
 if __name__ == '__main__':
